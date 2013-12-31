acc7049264c5922197a055a03e5d030892512780
Anand Doshi <anand@erpnext.com>
2013-12-31 11:15:13 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.0.1'
+
+setup(
+    name='shopping_cart',
+    version=version,
+    description='Online Shopping Cart integrated with ERPNext',
+    author='Web Notes Technologies',
+    author_email='info@erpnext.com',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=("webnotes",),
+)
