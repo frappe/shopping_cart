cd46370355ac53b078e379cc4c9b838eaa0e49da
Anand Doshi <anand@erpnext.com>
2014-04-16 15:23:22 +0530

    Login fix

--- shopping_cart/utils.py
+++ shopping_cart/utils.py
@@ -7,19 +7,27 @@ import frappe
 import frappe.defaults
 from frappe.utils import cint
 
+def show_cart_count():
+	if (frappe.db.get_default("shopping_cart_enabled") and
+		frappe.db.get_value("User", frappe.session.user, "user_type") == "Website User"):
+		return True
+
+	return False
+
 def set_cart_count(login_manager):
-	if frappe.db.get_value("User", frappe.session.user, "user_type") == "Website User":
+	if show_cart_count():
 		from shopping_cart.shopping_cart.cart import set_cart_count
 		set_cart_count()
-		
+
 def clear_cart_count(login_manager):
-	frappe.local.cookie_manager.delete_cookie("cart_count")
-	
+	if show_cart_count():
+		frappe.local.cookie_manager.delete_cookie("cart_count")
+
 def update_website_context(context):
 	post_login = []
 	cart_enabled = cint(frappe.db.get_default("shopping_cart_enabled"))
 	context["shopping_cart_enabled"] = cart_enabled
-	
+
 	if cart_enabled:
 		post_login += [
 			{"label": "Cart", "url": "cart", "icon": "icon-shopping-cart", "class": "cart-count"},
@@ -35,5 +43,5 @@ def update_website_context(context):
 		{"label": "Shipments", "url": "shipments", "icon": "icon-truck"},
 		{"class": "divider"}
 	]
-	
+
 	context["post_login"] = post_login + context.get("post_login", [])
