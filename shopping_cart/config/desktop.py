b2826ced5b185af03f73881110039693a19ab91b
Nabin Hait <nabinhait@gmail.com>
2014-05-16 20:01:18 +0530

    traslation issue fixed in desktop

--- shopping_cart/config/desktop.py
+++ shopping_cart/config/desktop.py
@@ -1,9 +1,10 @@
-data = {
-	"Shopping Cart": {
-		"color": "#B7E090",
-		"icon": "icon-shopping-cart",
-		"label": "Shopping Cart",
-		"link": "Form/Shopping Cart Settings",
-		"type": "module"
+def get_data():
+	return {
+		"Shopping Cart": {
+			"color": "#B7E090",
+			"icon": "icon-shopping-cart",
+			"label": "Shopping Cart",
+			"link": "Form/Shopping Cart Settings",
+			"type": "module"
+		}
 	}
-}
