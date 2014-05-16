f27b5a4e0dddbbccb5bcef324f14d77e33681760
Anand Doshi <anand@erpnext.com>
2014-05-16 23:32:51 +0530

    Fixed config/desktop.py

--- shopping_cart/config/desktop.py
+++ shopping_cart/config/desktop.py
@@ -1,9 +1,11 @@
+from frappe import _
+
 def get_data():
 	return {
 		"Shopping Cart": {
 			"color": "#B7E090",
 			"icon": "icon-shopping-cart",
-			"label": "Shopping Cart",
+			"label": _("Shopping Cart"),
 			"link": "Form/Shopping Cart Settings",
 			"type": "module"
 		}
