511ae7f7dca1edcda4920b7c92b63b9064845c33
Nabin Hait <nabinhait@gmail.com>
2014-05-19 18:17:39 +0530

    Server side onload functionality in multiple docs. Fixed #1623

--- shopping_cart/shopping_cart/doctype/shopping_cart_settings/shopping_cart_settings.js
+++ shopping_cart/shopping_cart/doctype/shopping_cart_settings/shopping_cart_settings.js
@@ -3,8 +3,8 @@
 
 $.extend(cur_frm.cscript, {
 	onload: function() {
-		if(cur_frm.doc.__quotation_series) {
-			cur_frm.fields_dict.quotation_series.df.options = cur_frm.doc.__quotation_series;
+		if(cur_frm.doc.__onload && cur_frm.doc.__onload.quotation_series) {
+			cur_frm.fields_dict.quotation_series.df.options = cur_frm.doc.__onload.quotation_series;
 		}
 	}
 });
